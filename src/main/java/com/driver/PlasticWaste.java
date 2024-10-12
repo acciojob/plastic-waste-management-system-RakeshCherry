d7bf5e96b2f276eec06caa9b68e22363a1b8d95a
RAKESH M B <118419906+RakeshCherry@users.noreply.github.com>
2024-10-12 13:40:09 +0530

    Solved PlasticWaste.java

--- src/main/java/com/driver/PlasticWaste.java
+++ src/main/java/com/driver/PlasticWaste.java
@@ -1,9 +1,7 @@
-package com.driver;
-
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
-import lombok.ToString;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -11,38 +9,57 @@ import java.util.Map;
 @Getter
 @Setter
 @AllArgsConstructor
-@ToString
+@NoArgsConstructor
 public class PlasticWaste {
 
-    private Map<String, Double> plasticWasteMap;
+    // A private map to store plastic types and their corresponding weights
+    private Map<String, Double> plasticWasteMap = new HashMap<>();
 
-    public PlasticWaste() {
-        this.plasticWasteMap = new HashMap<>();
+    // Method to add a new type of plastic waste
+    public void addPlasticType(String plasticType, double weight) {
+        plasticWasteMap.put(plasticType, weight);
+        System.out.println("Stored data for " + plasticType + ".");
     }
 
-    public void addPlasticType(String type, double weight) {
-        plasticWasteMap.put(type, weight);
-    }
-
-    public void updatePlasticWeight(String type, double newWeight) {
-        if (plasticWasteMap.containsKey(type)) {
-            plasticWasteMap.put(type, newWeight);
+    // Method to update the weight of an existing type of plastic waste
+    public void updatePlasticWeight(String plasticType, double weight) {
+        if (plasticWasteMap.containsKey(plasticType)) {
+            plasticWasteMap.put(plasticType, weight);
+            System.out.println("Updated weight for " + plasticType + " to " + weight + " kg.");
+        } else {
+            System.out.println(plasticType + " does not exist. Please add it first.");
         }
     }
 
+    // Method to get the total weight of all plastic waste
     public double getTotalWeight() {
         return plasticWasteMap.values().stream().mapToDouble(Double::doubleValue).sum();
     }
 
-    public double getWeightByType(String type) {
-        return plasticWasteMap.getOrDefault(type, 0.0);
+    // Method to get the weight of a specific type of plastic
+    public double getPlasticWeight(String plasticType) {
+        return plasticWasteMap.getOrDefault(plasticType, 0.0);
     }
 
-    public String getTypeWithHighestWeight() {
+    // Method to get the type of plastic with the highest weight
+    public String getHeaviestPlasticType() {
         return plasticWasteMap.entrySet().stream()
                 .max(Map.Entry.comparingByValue())
                 .map(Map.Entry::getKey)
-                .orElse(null);
+                .orElse("No plastic waste recorded.");
+    }
+
+    public static void main(String[] args) {
+        PlasticWaste plasticWaste = new PlasticWaste();
+
+        // Sample Input
+        plasticWaste.addPlasticType("PET", 50.5);
+        plasticWaste.addPlasticType("HDPE", 35.2);
+        plasticWaste.addPlasticType("PVC", 75.8);
+
+        // Sample Output
+        System.out.println("Total weight of plastic waste: " + plasticWaste.getTotalWeight() + " kg");
+        System.out.println("Weight of HDPE: " + plasticWaste.getPlasticWeight("HDPE") + " kg");
+        System.out.println("Heaviest plastic type: " + plasticWaste.getHeaviestPlasticType());
     }
 }
-
